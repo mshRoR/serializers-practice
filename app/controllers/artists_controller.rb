9f3353c1af97ed82cfdd7a5ea208c82de01361b0
Shajed <shajed_msh@yahoo.com>
2016-09-23 15:08:31 +0600

    new action created to artist controller and add to routes file

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,10 @@
 class ArtistsController < ApplicationController
 
   def index
-    render json: Artist.all
+    render json: ArtistsPresenter.new(Artist.all)
+  end
+
+  def show_albums
+    render json: AlbumsPresenter.new(Album.all)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'artists#index'
 
-  resources :artists
+  resources :artists, only: [:index]
+  get 'show_albums', to: 'artists#show_albums'
 end
